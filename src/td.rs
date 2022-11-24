cc7259807185fc52ceeebed5261f9963abb5ab26
Edoardo Morandi <morandidodo@gmail.com>
2022-11-24 09:41:28 +0100

    docs: add docs to lib

--- src/td.rs
+++ src/td.rs
@@ -1,7 +1,15 @@
+//! An stand-alone implementation of a Thing Description.
+//!
+//! This is a limited implementation to not rely on [`wot-td`] while testing it, use it to
+//! implement Things instead!
+//!
+//! Note: the implementation is incomplete.
+
 use std::{borrow::Cow, collections::HashMap, ops::Deref};
 
 use serde::{Deserialize, Deserializer};
 
+/// A simple wrapper to a `Cow<'static, str>`.
 #[derive(Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd, Deserialize)]
 pub struct CowStr(pub Cow<'static, str>);
 
@@ -25,42 +33,67 @@ impl Deref for CowStr {
     }
 }
 
+/// Possible security schemes.
 #[derive(Debug, Eq, PartialEq, Deserialize)]
 pub enum SecurityScheme {
+    /// No security scheme.
     #[serde(rename = "nosec")]
     NoSecurityScheme,
 }
 
+/// A Thing Description.
 #[derive(Debug, PartialEq, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ThingDescription {
+    /// The id.
     #[serde(default)]
     pub id: CowStr,
+
+    /// The title.
     #[serde(default)]
     pub title: CowStr,
+
+    /// The security string.
     #[serde(default)]
     pub security: CowStr,
+
+    /// The security definitions.
     pub security_definitions: HashMap<CowStr, SecuritySchemeDefinition>,
+
+    /// The JSON-LD @type.
     #[serde(rename = "@type")]
     pub attype: OneOrMany<CowStr>,
+
+    /// The description.
     #[serde(default)]
     pub description: CowStr,
+
+    /// The properties.
     #[serde(default)]
     pub properties: HashMap<CowStr, PropertyAffordance>,
+
+    /// The actions.
     #[serde(default)]
     pub actions: HashMap<CowStr, ActionAffordance>,
+
+    /// The events.
     #[serde(default)]
     pub events: HashMap<CowStr, EventAffordance>,
+
+    /// The Thing-level forms.
     #[serde(default)]
     pub forms: Vec<Form>,
 }
 
+/// The security scheme definitions.
 #[derive(Debug, Eq, PartialEq, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct SecuritySchemeDefinition {
+    /// The security scheme.
     pub scheme: SecurityScheme,
 }
 
+/// A wrapper to represent one or many elements using a [`Vec`].
 #[derive(Clone, Eq, PartialEq, Debug)]
 pub struct OneOrMany<T>(pub Vec<T>);
 
@@ -113,46 +146,72 @@ impl<T> From<T> for OneOrMany<T> {
     }
 }
 
+/// A partial interaction affordance.
+///
+/// It does not contain the _human readable_ part.
 #[derive(Debug, Default, Eq, PartialEq, Deserialize)]
 pub struct PartialInteractionAffordance {
+    /// The affordance forms.
     #[serde(skip_serializing_if = "Vec::is_empty", default)]
     pub forms: Vec<Form>,
 }
 
+/// A full interaction affordance.
 #[derive(Debug, Default, Eq, PartialEq, Deserialize)]
 pub struct InteractionAffordance {
+    /// The _partial_ interaction affordance.
     #[serde(flatten)]
     pub interaction: PartialInteractionAffordance,
+
+    /// The human readable fields.
     #[serde(flatten)]
     pub human_readable: HumanReadable,
 }
 
+/// A property affordance.
 #[derive(Debug, Deserialize)]
 pub struct PropertyAffordance {
+    /// The partial interaction affordance.
     #[serde(flatten)]
     pub interaction: PartialInteractionAffordance,
+
+    /// A partial data schema.
     #[serde(flatten)]
     pub data_schema: PartialDataSchema,
+
+    /// The human readable fields.
     #[serde(flatten)]
     pub human_readable: HumanReadable,
 }
 
+/// An action affordance.
 #[derive(Debug, Default, Deserialize)]
 pub struct ActionAffordance {
+    /// The interaction affordance.
     #[serde(flatten)]
     pub interaction: InteractionAffordance,
+
+    /// The data schema for input fields.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub input: Option<DataSchema>,
+
+    /// The data schema for output fields.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub output: Option<DataSchema>,
+
+    /// Is it synchronous?
     #[serde(skip_serializing_if = "Option::is_none")]
     pub synchronous: Option<bool>,
 }
 
+/// An event affordance.
 #[derive(Debug, Default, Deserialize)]
 pub struct EventAffordance {
+    /// The interaction affordance.
     #[serde(flatten)]
     pub interaction: InteractionAffordance,
+
+    /// The data schema.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub data: Option<DataSchema>,
 }
@@ -257,49 +316,79 @@ impl PartialEq for EventAffordance {
     }
 }
 
+/// Set of human readable fields.
 #[derive(Debug, Default, Eq, PartialEq, Deserialize)]
 pub struct HumanReadable {
+    /// The JSON-LD @type.
     #[serde(rename = "@type", skip_serializing_if = "CowStr::is_empty", default)]
     pub attype: CowStr,
+
+    /// The title.
     #[serde(skip_serializing_if = "CowStr::is_empty", default)]
     pub title: CowStr,
+
+    /// The description.
     #[serde(skip_serializing_if = "CowStr::is_empty", default)]
     pub description: CowStr,
 }
 
+/// A form component.
 #[derive(Debug, Eq, PartialEq, Deserialize)]
 pub struct Form {
+    /// The hyper-reference of the form.
     pub href: CowStr,
+
+    /// The operation(s) of the form.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub op: Option<OneOrMany<Operation>>,
+
+    /// The subprotocol.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub subprotocol: Option<CowStr>,
 }
 
+/// A data schema.
 #[derive(Debug, PartialEq, Deserialize)]
 pub struct DataSchema {
+    /// The partial data schema.
     #[serde(flatten)]
     pub data_schema: PartialDataSchema,
+
+    /// The human readable fields.
     #[serde(flatten)]
     pub human_readable: HumanReadable,
 }
 
+/// A partial data schema.
 #[derive(Debug, PartialEq, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct PartialDataSchema {
+    /// The data schema subtype.
     #[serde(flatten)]
     pub subtype: DataSchemaSubtype,
+
+    /// The unit.
     #[serde(skip_serializing_if = "CowStr::is_empty", default)]
     pub unit: CowStr,
+
+    /// Is it read only?
     pub read_only: Option<bool>,
+
+    /// Is it write only?
     pub write_only: Option<bool>,
+
+    /// Is it observable?
     #[serde(default)]
     pub observable: bool,
 }
 
+/// A helper structure for default data schema fields.
 #[derive(Debug, Clone, Copy)]
 pub struct PartialDataSchemaDefaults {
+    /// Is it read only?
     pub read_only: &'static Option<bool>,
+
+    /// Is it write only?
     pub write_only: &'static Option<bool>,
 }
 
@@ -313,6 +402,7 @@ impl Default for PartialDataSchemaDefaults {
 }
 
 impl PartialDataSchema {
+    /// Checks whether two partial data schemas are equivalent.
     pub fn is_equivalent(&self, other: &Self, defaults: Option<PartialDataSchemaDefaults>) -> bool {
         self.subtype.is_equivalent(&other.subtype)
             && self.unit == other.unit
@@ -328,6 +418,7 @@ impl PartialDataSchema {
 }
 
 impl DataSchema {
+    /// Checks whether two data schemas are equivalent.
     pub fn is_equivalent(&self, other: &Self) -> bool {
         self.data_schema.is_equivalent(&other.data_schema, None)
             && self.human_readable == other.human_readable
@@ -342,15 +433,29 @@ fn are_equivalent_opt_data_schema(left: &Option<DataSchema>, right: &Option<Data
     }
 }
 
+/// Data schema subtypes.
 #[derive(Debug, PartialEq, Deserialize)]
 #[serde(tag = "type", rename_all = "camelCase")]
 pub enum DataSchemaSubtype {
+    /// An object data schema.
     Object(ObjectDataSchema),
+
+    /// An array data schema.
     Array,
+
+    /// A string data schema.
     String,
+
+    /// A number data schema.
     Number(NumberDataSchema),
+
+    /// An integer data schema.
     Integer(IntegerDataSchema),
+
+    /// A boolean data schema.
     Boolean,
+
+    /// A null data schema.
     Null,
 }
 
@@ -369,26 +474,38 @@ impl DataSchemaSubtype {
     }
 }
 
+/// An integer data schema.
 #[derive(Debug, Default, Eq, PartialEq, Deserialize)]
 pub struct IntegerDataSchema {
+    /// The minimum value.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub minimum: Option<i64>,
+
+    /// The maximum value.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub maximum: Option<i64>,
 }
 
+/// A number data schema.
 #[derive(Debug, Default, PartialEq, Deserialize)]
 pub struct NumberDataSchema {
+    /// The minimum value.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub minimum: Option<f64>,
+
+    /// The maximum value.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub maximum: Option<f64>,
 }
 
+/// An object data schema.
 #[derive(Debug, Default, PartialEq, Deserialize)]
 pub struct ObjectDataSchema {
+    /// The properties of the object.
     #[serde(skip_serializing_if = "HashMap::is_empty")]
     pub properties: HashMap<CowStr, DataSchema>,
+
+    /// The set of required properties, by key.
     #[serde(skip_serializing_if = "Vec::is_empty")]
     pub required: Vec<CowStr>,
 }
@@ -409,25 +526,61 @@ impl ObjectDataSchema {
     }
 }
 
+/// A form operation.
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Deserialize)]
 #[serde(rename_all = "lowercase")]
 pub enum Operation {
+    /// Read a property.
     ReadProperty,
+
+    /// Write a property.
     WriteProperty,
+
+    /// Observe a property.
     ObserveProperty,
+
+    /// Unobserve a property.
     UnobserveProperty,
+
+    /// Invoke an action.
     InvokeAction,
+
+    /// Query an action.
     QueryAction,
+
+    /// Cancel an action.
     CancelAction,
+
+    /// Subscribe to an event.
     SubscribeEvent,
+
+    /// Unsubscribe from an event.
     UnsubscribeEvent,
+
+    /// Read all properties.
     ReadAllProperties,
+
+    /// Write to all properties.
     WriteAllProperties,
+
+    /// Read multiple properties.
     ReadMultipleProperties,
+
+    /// Write to multiple properties.
     WriteMultipleProperties,
+
+    /// Observer all properties.
     ObserveAllProperties,
+
+    /// Unobserve all properties.
     UnobserveAllProperties,
+
+    /// Subscribe to all events.
     SubscribeAllEvents,
+
+    /// Unsubscribe from all events.
     UnsubscribeAllEvents,
+
+    /// Query all actions.
     QueryAllActions,
 }
